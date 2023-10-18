a57abf28ab0e40a3389000f31dd726ed5cb72d65
panchSAHIL <125552170+panchSAHIL@users.noreply.github.com>
2023-10-18 12:24:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from the public directory (where your index.html is located)
-app.use(express.static('../public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
